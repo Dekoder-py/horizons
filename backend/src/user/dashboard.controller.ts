bd612acdca6801c3fd2f565367399e130355e0ea
techpixel <68567672+techpixel@users.noreply.github.com>
2026-02-05 23:30:10 -0500

    fix: prisma 7 imports

--- backend/src/user/dashboard.controller.ts
+++ backend/src/user/dashboard.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Headers, HttpCode, HttpException, HttpStatus, Query } from '@nestjs/common';
 import { AdminService } from './admin.service';
-import { PrismaClient } from '.prisma/client';
+import { PrismaClient } from '../../generated/prisma/client';
 
 @Controller('admin/dashboard')
 export class DashboardController {
